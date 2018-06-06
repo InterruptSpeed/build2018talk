af416ed65e0eac562ef9bedb67528a0247cedc0d
Steven Docherty <sdocherty@InterruptSpeed.com>
2018-06-06 12:51:54 -0400

    added countdown stream

--- demo1/Hubs/ApplicationHub.cs
+++ demo1/Hubs/ApplicationHub.cs
@@ -1,10 +1,29 @@
 using System.Threading.Tasks;
+using System.Threading.Channels;
 using Microsoft.AspNetCore.SignalR;
 
 namespace demo1 {
     public class ApplicationHub : Hub {
         public Task Send(ChatMessage message) {
             return Clients.All.SendAsync("Send", message.Message);
+        }
+
+        public ChannelReader<int> CountDown(int from) {
+            var channel = Channel.CreateUnbounded<int>();
+
+            _ = WriteToChannel(channel.Writer, from);
+
+            return channel.Reader;
+
+            async Task WriteToChannel(ChannelWriter<int> writer, int thing) {
+                for (int i = thing - 1; i >= 0 ; i--)
+                {
+                    await writer.WriteAsync(i);
+                    await Task.Delay(1000);
+                }
+
+                writer.Complete();
+            }
         }
     }
 }
